d0fcda1454deae5c6aaf17598bc123cdeb1a9018
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-09-01 02:08:15 +0300

    Update nov.js

--- nov.js
+++ nov.js
@@ -13,11 +13,11 @@ client.on('ready', () => {
   console.log(' Mob2 Bot Is Online')
   console.log('---------------')
   client.user.setStatus("Online")
-    client.user.setActivity(" Type -help",{type: 'WATCHING'})
+    client.user.setActivity(" Type **help",{type: 'WATCHING'})
 	
 });
 	
-            var prefix = "-" ;
+            var prefix = "**" ;
 	
 	
 	/*
@@ -31,7 +31,7 @@ npm install queue
 */
 client.on('message', msg => {
 
-    if (msg.content == '-play') {
+    if (msg.content == '**play') {
         if (msg.member.voiceChannel) {
 
      if (msg.member.voiceChannel.joinable) {
@@ -232,7 +232,7 @@ function play(guild, song) {
 	serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
 }//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
 
-const adminprefix = "-";//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+const adminprefix = "**";//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
 const devs = ['470500980342128650'];//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
 client.on('message', message => {//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
   var argresult = message.content.split(` `).slice(1).join(' ');//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
